82441f52690dd8e3273a146def020bc7ed0325b3
Matthias Derer <matthias.derer@esono.de>
2021-03-18 14:39:00 +0100

    extract output from data model into command

--- Command/UpgradeHelperCommand.php
+++ Command/UpgradeHelperCommand.php
@@ -4,9 +4,11 @@
 namespace ChameleonSystem\UpgradeHelperBundle\Command;
 
 
+use ChameleonSystem\UpgradeHelperBundle\DataModel\Warning;
 use ChameleonSystem\UpgradeHelperBundle\Parser\Parser;
 use ChameleonSystem\UpgradeHelperBundle\Validator\Validator;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -49,10 +51,40 @@ final class UpgradeHelperCommand extends Command
         $output->writeln("");
 
         foreach($warnings as $warning) {
-            $warning->format($output);
+            $this->writeWarning($warning, $output);
             $output->writeln("");
         }
 
         return 0;
+    }
+
+    /**
+     * writeWarning can be used to ouput the warning to the console
+     *
+     * @param OutputInterface $output
+     */
+    private function writeWarning(Warning $warning, OutputInterface $output): void
+    {
+        $outputStyle = new OutputFormatterStyle('magenta', 'default', ['bold']);
+        $output->getFormatter()->setStyle('warn', $outputStyle);
+        $outputStyle = new OutputFormatterStyle('red', 'default', ['bold']);
+        $output->getFormatter()->setStyle('error', $outputStyle);
+        $outputStyle = new OutputFormatterStyle('cyan', 'default');
+        $output->getFormatter()->setStyle('info', $outputStyle);
+
+        switch ($warning->type) {
+            case (Warning::TYPE_IMPLICIT):
+                $output->writeln('<warn>Warning: Implicit fetch from container</warn>');
+                $output->writeln('<info>The actual service could not be detected by the parser because it is not explicitly stated. Make sure that all services that could be loaded here are public.</info>');
+
+                break;
+            case (Warning::TYPE_NON_EXISTENT):
+                $output->writeln('<error>Error: Fetch of a non existing service from container</error>');
+                $output->writeln('<info>This service does not exist in the current container. If the service is defined, add `public="true"` as an attribute. If it is a synthetic service you might ignore this warning.</info>');
+                $output->writeln('Service: ' . $warning->call->service);
+                break;
+        }
+        $output->writeln('File: ' . $warning->call->file);
+        $output->writeln('Line: ' . $warning->call->line);
     }
 }
--- DataModel/Warning.php
+++ DataModel/Warning.php
@@ -29,35 +29,4 @@ final class Warning
      */
     public $type;
 
-    /**
-     * format can be used to ouput the warning to the console
-     *
-     * @param OutputInterface $output
-     */
-    public function format(OutputInterface $output): void
-    {
-        $outputStyle = new OutputFormatterStyle('magenta', 'default', ['bold']);
-        $output->getFormatter()->setStyle('warn', $outputStyle);
-        $outputStyle = new OutputFormatterStyle('red', 'default', ['bold']);
-        $output->getFormatter()->setStyle('error', $outputStyle);
-        $outputStyle = new OutputFormatterStyle('cyan', 'default');
-        $output->getFormatter()->setStyle('info', $outputStyle);
-
-
-        switch ($this->type) {
-            case (self::TYPE_IMPLICIT):
-                $output->writeln('<warn>Warning: Implicit fetch from container</warn>');
-                $output->writeln('<info>The actual service could not be detected by the parser because it is not explicitly stated. Make sure that all services that could be loaded here are public.</info>');
-
-                break;
-            case (self::TYPE_NON_EXISTENT):
-                $output->writeln('<error>Error: Fetch of a non existing service from container</error>');
-                $output->writeln('<info>This service does not exist in the current container. If the service is defined, add `public="true"` as an attribute. If it is a synthetic service you might ignore this warning.</info>');
-                $output->writeln('Service: ' . $this->call->service);
-                break;
-        }
-        $output->writeln('File: ' . $this->call->file);
-        $output->writeln('Line: ' . $this->call->line);
-    }
-
 }
